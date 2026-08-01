f5fe9033c3f173e15eef4f26189130ccfa238c2f
widherlions <widherl95@gmail.com>
2026-08-01 11:10:02 +0700

    Create products.js

---
+++ products.js
@@ -0,0 +1,20 @@
+const products = [
+  {
+    nama: "Anti Respirant Deodorant Kahf 45ml",
+    harga: "Rp27.000",
+    gambar: "images/deodorant.jpg",
+    wa: "6285794859861"
+  },
+  {
+    nama: "Parfum Kahf",
+    harga: "Rp45.000",
+    gambar: "images/parfum.jpg",
+    wa: "6285794859861"
+  },
+  {
+    nama: "Sabun Muka Kahf",
+    harga: "Rp35.000",
+    gambar: "images/sabun.jpg",
+    wa: "6285794859861"
+  }
+];
