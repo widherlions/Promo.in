a3c4dcc1ac20020f2ce5c19c2c05f7f64bb3ef16
widherlions <widherl95@gmail.com>
2026-08-01 15:07:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,10 +12,9 @@ let html="";
 
 data.forEach(item=>{
 html+=`
-<div class="card">
-<img src="${item.gambar}">
-<h3>${item.nama}</h3>
-<p>${item.harga}</p>
+<div class="aksi">
+<button onclick="tambahKeranjang(${item.id})">🛒 Tambah ke Keranjang</button>
+
 <a href="https://wa.me/${item.wa}?text=${encodeURIComponent(
 `Halo Admin 👋
 
@@ -27,6 +26,7 @@ Saya ingin membeli:
 Mohon informasi ketersediaannya.
 Terima kasih.`
 )}" target="_blank">Beli via WhatsApp</a>
+
 </div>
 `;
 });
