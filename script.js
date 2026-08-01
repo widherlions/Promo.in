1628fe033c3d50488a3bd3d2b5853459de069e1c
widherlions <widherl95@gmail.com>
2026-08-01 11:29:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-const container = document.getElementById("produk");
+let semuaProduk=[];
 
-products.forEach(item => {
+fetch("products.json")
+.then(res=>res.json())
+.then(data=>{
+semuaProduk=data;
+tampilkan(data);
+});
 
-container.innerHTML += `
+function tampilkan(data){
+let html="";
+
+data.forEach(item=>{
+html+=`
 <div class="card">
 <img src="${item.gambar}">
 <h3>${item.nama}</h3>
 <p>${item.harga}</p>
-
-<a href="https://wa.me/${item.wa}?text=Saya ingin membeli ${encodeURIComponent(item.nama)}">
-Pesan via WhatsApp
-</a>
-
+<a href="https://wa.me/${item.wa}?text=Saya ingin membeli ${encodeURIComponent(item.nama)}">Beli via WhatsApp</a>
 </div>
 `;
+});
 
+document.getElementById("products").innerHTML=html;
+}
+
+document.getElementById("search").addEventListener("input",function(){
+let cari=this.value.toLowerCase();
+
+let hasil=semuaProduk.filter(item=>
+item.nama.toLowerCase().includes(cari)
+);
+
+tampilkan(hasil);
 });
